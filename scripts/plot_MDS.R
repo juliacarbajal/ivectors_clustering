1395d0a463a4028c03f997185391379cfb1f896d
Julia C <carbajal.mjulia@gmail.com>
2016-07-12 19:39:54 +0200

    Added gender information.

--- scripts/plot_MDS.R
+++ scripts/plot_MDS.R
@@ -19,6 +19,7 @@ speakers = gsub(":.*", "", perUtterancenames)
 names(speakers) = perUtterancenames
 language = gsub("_.*", "", perUtterancenames)
 names(language) = perUtterancenames
+gender = c(rep("M",50),rep("F",50),rep("M",50),rep("F",50))
 
 # MDS ####
 
@@ -34,7 +35,7 @@ x   = fit$points[, 1] # MDS coordinate 1
 y   = fit$points[, 2] # MDS coordinate 2
 
 # 2. Organise in data frame
-mds.bil = data.frame(speakers, language, x, y)
+mds.bil = data.frame(speakers, language, gender, x, y)
 mds.bil$background = rep("Mixed", 200)
 mds.bil$LDA        = rep("pre-LDA", 200)
 mds.bil$x          = scale(mds.bil$x, center = FALSE, scale = sd(mds.bil$x)) #max(mds.bil$x, na.rm = TRUE))
@@ -50,7 +51,7 @@ fit = cmdscale(d, eig = T, k = 2)
 x   = fit$points[, 1]
 y   = fit$points[, 2]
 
-mds.bilLDA = data.frame(speakers, language, x, y)
+mds.bilLDA = data.frame(speakers, language, gender, x, y)
 mds.bilLDA$background = rep("Mixed", 200)
 mds.bilLDA$LDA        = rep("post-LDA", 200)
 mds.bilLDA$x = scale(mds.bilLDA$x, center = FALSE, scale = sd(mds.bilLDA$x)) #max(mds.bilLDA$x, na.rm = TRUE))
@@ -69,7 +70,7 @@ x   = fit$points[, 1] # MDS coordinate 1
 y   = fit$points[, 2] # MDS coordinate 2
 
 # 2.
-mds.xit = data.frame(speakers, language, x, y)
+mds.xit = data.frame(speakers, language, gender, x, y)
 mds.xit$background = rep("Xitsonga", 200)
 mds.xit$LDA        = rep("pre-LDA", 200)
 mds.xit$x = scale(mds.xit$x, center = FALSE, scale = sd(mds.xit$x)) #max(mds.xit$x, na.rm = TRUE))
@@ -85,7 +86,7 @@ fit = cmdscale(d, eig = T, k = 2)
 x   = fit$points[, 1]
 y   = fit$points[, 2]
 
-mds.xitLDA = data.frame(speakers, language, x, y)
+mds.xitLDA = data.frame(speakers, language, gender, x, y)
 mds.xitLDA$background = rep("Xitsonga", 200)
 mds.xitLDA$LDA        = rep("post-LDA", 200)
 mds.xitLDA$x   = scale(mds.xitLDA$x, center = FALSE, scale = sd(mds.xitLDA$x)) #max(mds.xitLDA$x, na.rm = TRUE))
@@ -104,7 +105,7 @@ x   = fit$points[, 1] # MDS coordinate 1
 y   = fit$points[, 2] # MDS coordinate 2
 
 # 2.
-mds.eng = data.frame(speakers, language, x, y)
+mds.eng = data.frame(speakers, language, gender, x, y)
 mds.eng$background = rep("English", 200)
 mds.eng$LDA        = rep("pre-LDA", 200)
 mds.eng$x   = scale(mds.eng$x, center = FALSE, scale = sd(mds.eng$x)) #max(mds.eng$x, na.rm = TRUE))
@@ -120,7 +121,7 @@ fit = cmdscale(d, eig = T, k = 2)
 x   = fit$points[, 1]
 y   = fit$points[, 2]
 
-mds.engLDA = data.frame(speakers, language, x, y)
+mds.engLDA = data.frame(speakers, language, gender, x, y)
 mds.engLDA$background = rep("English", 200)
 mds.engLDA$LDA        = rep("post-LDA", 200)
 mds.engLDA$x   = scale(mds.engLDA$x, center = FALSE, scale = sd(mds.engLDA$x)) #max(mds.engLDA$x, na.rm = TRUE))
@@ -142,8 +143,9 @@ saveplot = 0 # Set to 1 if saving plot
 if (saveplot == 1) png("figures/MDS_scaleSD.png", width=13*ppi, height=7*ppi, res=ppi)
 ggplot(MDS.data, aes(x = x, y = y))+
   geom_point(aes(color = language, shape = language), size = 2.5) +
+  #geom_point(aes(color = gender, shape = language), size = 2.5) + # Un-comment this line if coloring by gender
   facet_grid(LDA ~ background) +
-  scale_color_manual(name = "Language", values = c("black", "grey48")) +
+  scale_color_manual(name = "Language", values = c("black", "grey48")) + # Comment this line if coloring by gender
   theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
   theme(text = element_text(size = 18)) +
   scale_shape_discrete(name = "Language") +
